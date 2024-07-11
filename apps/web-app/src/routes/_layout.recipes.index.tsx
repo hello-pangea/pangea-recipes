46e6823e408ebe800a56272196a9785a28f2966d
Reece Carolan <reece@mycopilot.com>
2024-07-10 19:42:08 -0700

    misc: ensure recipes query

--- apps/web-app/src/routes/_layout.recipes.index.tsx
+++ apps/web-app/src/routes/_layout.recipes.index.tsx
@@ -1,6 +1,14 @@
 import { RecipesPage } from '#src/features/recipes/RecipesPage';
+import { getListRecipesQueryOptions } from '@open-zero/features';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_layout/recipes/')({
+  loader: async ({ context }) => {
+    await context.queryClient.ensureQueryData(
+      getListRecipesQueryOptions({
+        userId: context.auth.user?.id ?? '',
+      }),
+    );
+  },
   component: RecipesPage,
 });
