ac85d31ce8e07ece56ce9dee50db471effda9d40
Reece Carolan <reece@mycopilot.com>
2025-01-26 12:07:26 -0800

    fix: prisma export breaking things

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -9,5 +9,3 @@ export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 if (process.env['NODE_ENV'] !== 'production') {
   globalForPrisma.prisma = prisma;
 }
-
-export * from '@prisma/client';
--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,5 +1,5 @@
-// Exports generated types from prisma
-export * from '../prisma/generated/client/index.js';
-
 // Exports instance of prisma
 export { prisma } from './client.js';
+
+// Exports generated types from prisma
+export * from '../prisma/generated/client/index.js';
