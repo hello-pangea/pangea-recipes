61166dce85d60b063e2c4ce1e2d445489ad53fb5
Reece Carolan <reececarolan@gmail.com>
2025-06-03 23:01:42 -0700

    perf: lazy load landing page

--- apps/web/src/routes/index.tsx
+++
@@ -1,6 +0,0 @@
-import { HomePage } from '#src/features/marketing/HomePage';
-import { createFileRoute } from '@tanstack/react-router';
-
-export const Route = createFileRoute('/')({
-  component: HomePage,
-});
