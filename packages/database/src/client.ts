26ce23a9459066092e1968d1864d233be3c893ee
Reece Carolan <reececarolan@gmail.com>
2025-07-04 12:39:20 -0700

    chore: use prisma-client provider

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated-client/index.js';
+import { PrismaClient } from './generated-prisma-client/client.js';
 
 const globalForPrisma = global as unknown as {
   prisma?: PrismaClient;
