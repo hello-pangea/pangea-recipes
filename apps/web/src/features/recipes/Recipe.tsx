d6f720eb70a3f681650a2499bc8ec5981bc89f86
Reece Carolan <reececarolan@gmail.com>
2025-07-04 22:47:43 -0700

    fix ssr issue

--- apps/web/src/features/recipes/Recipe.tsx
+++ apps/web/src/features/recipes/Recipe.tsx
@@ -1,3 +1,4 @@
+import { useIsMounted } from '#src/hooks/useIsMounted';
 import { getNumberFromInput } from '#src/utils/getNumberFromInput';
 import { secondsToTimeString } from '#src/utils/timeFormatting';
 import AddRoundedIcon from '@mui/icons-material/AddRounded';
@@ -56,6 +57,7 @@ export function Recipe({ readOnly, recipeId }: Props) {
       enqueueSnackbar('Failed to keep screen awake', { variant: 'error' });
     },
   });
+  const isMounted = useIsMounted();
   const [servingsModifier, setServingsModifier] = useState(
     String(recipe.servings ?? 1),
   );
@@ -336,7 +338,8 @@ export function Recipe({ readOnly, recipeId }: Props) {
           </Grid>
         )}
       </Grid>
-      {isWakeLockSupported && (
+      {/* SSR fix */}
+      {isWakeLockSupported && isMounted && (
         <FormGroup sx={{ mb: 2 }}>
           <FormControlLabel
             control={
