d9d5d1595f968bf9c87b37fb4e55b7e6ea8db3fe
Reece Carolan <reececarolan@gmail.com>
2025-11-16 13:56:01 -0800

    fallback

--- apps/web/src/config/config.ts
+++ apps/web/src/config/config.ts
@@ -4,7 +4,7 @@ const envSchema = z.object({
   VITE_API_URL: z.string(),
   VITE_GOOGLE_TAG_ID: z.string().optional(),
   VITE_GOOGLE_TAG_CONVERSION_DESTINATION: z.string().optional(),
-  PROD: z.boolean(),
+  PROD: z.boolean().catch(false),
 });
 
 export type Env = z.infer<typeof envSchema>;
