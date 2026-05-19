1cd0cfa58b7895d1028ed0e54e69b03737327206
Reece Carolan <reececarolan@gmail.com>
2026-05-19 01:00:32 -0400

    add log

--- apps/web/src/config/config.ts
+++ apps/web/src/config/config.ts
@@ -1,5 +1,12 @@
 import { z } from 'zod';
 
+console.log({
+  // @ts-expect-error fjiorefjroie f
+  // oxlint-disable-next-line typescript/no-unsafe-assignment typescript/no-unsafe-member-access
+  process: process.env,
+  meta: import.meta.env,
+});
+
 const envSchema = z.object({
   VITE_API_URL: z.string(),
   VITE_GOOGLE_TAG_ID: z.string().optional(),
